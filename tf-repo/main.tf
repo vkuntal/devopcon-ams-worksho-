27a12fc886454af3c2bb3ac30d0db24b47736f1a
Vijit Kuntal <47222485+vkuntal@users.noreply.github.com>
2026-04-21 12:40:23 +0000

    tet

--- tf-repo/main.tf
+++ tf-repo/main.tf
@@ -1,5 +1,5 @@
 resource "github_repository" "repo_devops" {
   name        = var.repository_name
   description = var.repository_description
-  visiblity     = var.visibility
+  visibility     = var.visibility
 }
